45017440bf967bb02098276122869d53d3ca49b5
goransasic <goran.sasic@servicehouse.nl>
2022-01-25 22:24:11 +0100

    [NMS-10] - review fix

--- src/main/java/com/opdev/repository/PostRepository.java
+++ src/main/java/com/opdev/repository/PostRepository.java
@@ -5,14 +5,13 @@ import com.opdev.model.company.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Modifying
     @Query("delete from Post p where p.id = :postId and p.company.id = :companyId")
